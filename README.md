8de94a80b57b390c789d862eed3f1dac23502cfe
ygrachov <iurii.grachov@gmail.com>
2023-01-15 11:08:40 +0100

    added documentation and cleaned views.py

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+### <li> Backend: Django 4.1.4, Python 3.9
+### <li> Database: SQLLite
+### <li> Frontend: HTML-5, Bootstrap-4, JavaScript
+
+
+### manage.py:
+The manage.py file is a command-line utility that allows you to interact with this application, primarily during the development process
+
+### models.py:
+models.py defines two models</p>
+<li>a Django model called CreateInput. The model has several fields, each representing a different aspect of a call center simulation.
+<li>class GlobalResults is a Django model that represents the results of a call center simulation. The class has several fields that store different aspects of the simulation results
+
 ### views.py:
 <li>The InputForm is a ModelForm used to create a form for the CreateInput model. 
 <li>The index function returns an empty context dictionary and renders the 'simulator/index.html' template. 
@@ -8,3 +21,30 @@
 <li>The launch function starts the simulation. 
 <li>The Results class renders the output page when the simulation is completed. 
 <li>The export_csv function exports the data from the GlobalResults model as a CSV file and returns it as an HTTP response
+
+### urls.py:
+The app_name variable is set to 'simulator' which is used to uniquely identify the URLs in this file among others in the project</p>
+
+The urlpatterns list contains a series of path objects, each of which maps a URL to a view function. The URLs and the corresponding views are:</p>
+
+<li>'' (the root URL of the project) is mapped to the index view. The name of this URL is 'main'.
+<li>'form/' is mapped to the GetVars view, which is a class-based view. The name of this URL is 'form'.
+<li>'launch' is mapped to the launch view. The name of this URL is 'launch'.
+<li>'export/' is mapped to the display_results view. The name of this URL is 'export'.
+<li>'download_csv' is mapped to the export_csv view. The name of this URL is 'csv'.</li></p>
+When a user navigates to one of these URLs in their web browser, the corresponding view function will be executed, and the resulting HTML will be displayed in the browser.
+
+### forms.py:
+This file contains the InputForm class, which is a Django ModelForm that is used to handle user input for the CreateInput model.</p>
+This class is a ModelForm that is generated from the CreateInput model.</p>
+The fields attribute is set to 'all', which means that all fields in the CreateInput model will be included in the form. 
+
+### templates: 
+This folder contains the HTML templates that define the structure and layout of the pages that are displayed to the user. 
+
+### static:
+The static directory contains a collection of images used in the layout of pages.
+
+### templatetags:
+This directory contains a custom method which allows for the rendering of nicely formatted content from the database.
+
--- simulator/views.py
+++ simulator/views.py
@@ -1,27 +1,33 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
+from django.shortcuts import render, redirect
 from django.forms import ModelForm
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 import simpy
 from . import models
 import random
-from django.db.models import Avg, Max, Sum, Count
-from django.views.generic import UpdateView, TemplateView, FormView
+from django.views.generic import FormView
 from datetime import datetime
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 import uuid
 import csv
 
 
 class Uuid:
+    """Generates a unique ID for user input, using UUID standard to ensure no collisions.
+    The unique ID is stored as a string object self.id"""
     def __init__(self):
         self.id = uuid.uuid4().hex
 
 
 num = Uuid()
 
+
 class InputForm(ModelForm):
+    """ A form class used to validate user input for a simulation. Inherits from Django's ModelForm and uses
+    the CreateInput model. Includes fields for all input variables, as well as a clean method for validating input
+    values."""
     class Meta:
+        """ A nested class that defines metadata for the InputForm class.
+        Specifies the model used for the form and the fields included in the form"""
         model = models.CreateInput
         fields = ['number_of_simulations',
                   'line_numbers',
@@ -51,6 +57,7 @@ class InputForm(ModelForm):
                   'c_l']
 
     def clean(self):
+        """validates user's input for the logic required to make the model works """
         cleaned_data = super().clean()
         shift_time = cleaned_data.get('shift_time')
         take_high = cleaned_data.get('take_high')
@@ -117,16 +124,21 @@ class InputForm(ModelForm):
 
 
 def index(request):
+    """Handles request for main page and returns rendered template"""
     context = {}
     return render(request, 'simulator/index.html', context)
 
 
 class GetVars(FormView):
+    """A FormView class that handles the rendering and processing of the input form.
+    If input is valid it is being written to the database (table Createinput)"""
     form_class = InputForm
     template_name = 'simulator/form.html'
     success_url = reverse_lazy('simulator:launch')
 
     def form_valid(self, form):
+        """Calls the clean method of the InputForm and handles a valid form submission by saving the data to
+        the database and redirecting to the 'success_url'"""
         if form.is_valid():
             instance = form.save(commit=False)
             uuid = num.id
@@ -139,8 +151,64 @@ class GetVars(FormView):
             return super().form_invalid(form)
 
 
+def launch(request):
+    """Handles the execution of the simulation model, triggered by a valid form submission.
+    This method uses all the necessary input from the user to initialize and run the simulation environment."""
+
+    start = datetime.now()
+    for simulation_number in range(models.CreateInput.objects.values('number_of_simulations').last()
+                                   ['number_of_simulations']):
+        print(f'run {simulation_number}')
+        CallCenter.env = simpy.Environment()
+        call_center = CallCenter(simulation_number, num.id)
+        call_center.run()
+    finish = datetime.now()
+    print(f' simulation took {finish - start}')
+    return redirect('simulator:export')
+
+
+def display_results(request):
+    """Renders the content of the resulting page after the simulation is completed. Displays the input values used
+    in the simulation and provides a link to download the simulation log file."""
+    template_name = 'simulator/export.html'
+    myvars = models.CreateInput.objects.filter(uuid=num.id).values().last()
+    fields = InputForm()
+    return render(request,  template_name=template_name, context={'myvars': myvars, 'form': fields})
+
+
+def export_csv(request):
+    """writes simulation log file in '.csv' format and returns it as a response to be downloaded by the user"""
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="simulation_log.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['shift_started', 'shift_finished', 'run', 'attempt_no', 'call_no', 'batch', 'queue', 'spin',
+                     'capacity', 'attempt_started', 'if_unreachable',
+                     'if_not_answering', 'answer_time', 'amd_time', 'if_dropped', 'wait_before_drop', 'wait_time',
+                     'talk_time', 'clerical_time'])
+
+    results = models.GlobalResults.objects.filter(uuid=num.id).values_list('shift_started', 'shift_finished', 'run',
+                                                                           'attempt_no', 'call_no', 'batch', 'queue',
+                                                                           'spin', 'capacity',
+                                                                           'attempt_started', 'if_unreachable',
+                                                                           'if_not_answering', 'answer_time',
+                                                                           'amd_time', 'if_dropped',
+                                                                           'wait_before_drop', 'wait_time', 'talk_time',
+                                                                           'clerical_time')
+
+    for result in results:
+        writer.writerow(result)
+
+    models.GlobalResults.objects.filter(uuid=num.id).delete()
+    models.CreateInput.objects.filter(uuid=num.id).delete()
+
+    return response
+
+
 class CallCenter:
-    """represents call center """
+    """The CallCenter class represents the logic of the call center business process. A unique instance of the class
+    is created for each simulation run (variable 'simulation_number'). All variables for the init method are retrieved
+     from the database (table CreateInput). To avoid issues with multiple users running the app simultaneously, each
+     user is identified by a unique uuid, which is generated from the Uuid class"""
     def __init__(self, simulation_number, uuid):
         self.uuid = uuid
         self.name = 0
@@ -165,7 +233,7 @@ class CallCenter:
         self.unreachable_m = models.CreateInput.objects.values('unreachable_m').last()['unreachable_m']
         # time between point of call starts ringing and call gets answered or cancelled (high, low, mode)
         self.ring_time_h = models.CreateInput.objects.values('ring_time_h').last()['ring_time_h']
-        self.ring_time_l =models.CreateInput.objects.values('ring_time_l').last()['ring_time_l']
+        self.ring_time_l = models.CreateInput.objects.values('ring_time_l').last()['ring_time_l']
         self.ring_time_m = models.CreateInput.objects.values('ring_time_m').last()['ring_time_m']
         # share of reached customers from those who received a call (high, low, mode)
         self.reach_rate_h = models.CreateInput.objects.values('reach_rate_h').last()['reach_rate_h']
@@ -173,19 +241,25 @@ class CallCenter:
         self.reach_rate_m = models.CreateInput.objects.values('reach_rate_m').last()['reach_rate_m']
         # answering machine clearance duration parameters (high, low, mode)
         self.d_h = models.CreateInput.objects.values('d_h').last()['d_h']
-        self.d_l =models.CreateInput.objects.values('d_l').last()['d_l']
-        self.d_m =models.CreateInput.objects.values('d_m').last()['d_m']
+        self.d_l = models.CreateInput.objects.values('d_l').last()['d_l']
+        self.d_m = models.CreateInput.objects.values('d_m').last()['d_m']
         # talk time parameters (high, low)
         self.t_h = models.CreateInput.objects.values('t_h').last()['t_h']
         self.t_l = models.CreateInput.objects.values('t_l').last()['t_l']
         # clerical time parameters (high, low)
         self.c_h = models.CreateInput.objects.values('c_h').last()['c_h']
-        self.c_l =models.CreateInput.objects.values('c_l').last()['c_l']
+        self.c_l = models.CreateInput.objects.values('c_l').last()['c_l']
         self.queue = 0
         self.start = 0
         self.finish = 0
 
     def dial(self):
+        """The dial method simulates the behavior of an autodialer and applies the logic of a funnel. It selects a
+        portion of the call list (batch) based on the number of available lines and the current queue. If a number is
+        unreachable, it is removed from the call list. If it is reached, it may result either in a list of answered
+        calls ('talk') or not. If the attempt is not in the 'talk' list, the phone number becomes the last element of
+        the call list. If it is in the 'talk' list, the accepting_call method is applied to that. Each batch is
+        written to DB"""
         record1 = models.GlobalResults(shift_started=self.env.now)
         record1.uuid = self.uuid
         record1.save()
@@ -216,7 +290,7 @@ class CallCenter:
                 record.save()
             self.call_list = [i for i in self.call_list if i not in batch]
             yield self.env.timeout(random.triangular(high=self.take_high, low=self.take_low,
-                                                        mode=self.take_mode))
+                                                     mode=self.take_mode))
             unreached = random.sample(population=batch, k=round(random.triangular(high=self.unreachable_h,
                                                                                   low=self.unreachable_l,
                                                                                   mode=self.unreachable_m) *
@@ -227,7 +301,7 @@ class CallCenter:
                 update_rec.save()
             batch = [i for i in batch if i not in unreached]
             yield self.env.timeout(random.triangular(high=self.ring_time_h, low=self.ring_time_l,
-                                                        mode=self.ring_time_m))
+                                                     mode=self.ring_time_m))
             talks = random.sample(population=batch, k=round(len(batch) * random.triangular(high=self.reach_rate_h,
                                                                                            low=self.reach_rate_l,
                                                                                            mode=self.reach_rate_m)))
@@ -241,13 +315,18 @@ class CallCenter:
                 call = IncomingCall(i)
                 self.env.process(self.accepting_call(call, answer_time))
             self.batch += 1
-            print(f'batch# {self.batch} took {len(batch)} at {start} & queue is {self.queue} and call_list is {len(self.call_list)}')
-        final_record = models.GlobalResults(shift_finished =self.env.now)
+        final_record = models.GlobalResults(shift_finished=self.env.now)
         final_record.uuid = self.uuid
         final_record.save()
 
-
     def accepting_call(self, call, answer_time):
+        """It simulates how a call is handled. When a customer answers, the system checks if it is an answering
+        machine for a certain duration. If the duration exceeds the customer's patience, the call is dropped and
+        the phone number becomes the last element of the call list. If the call is not dropped, an agent is requested
+        and the system waits for them until the customer's patience expires. If patience ends, the call is dropped and
+        the phone number becomes the last element of the call list. If the call is not dropped, it is handled during the
+        talk_time. The agent is then released and available for the next customer in the queue after the clerical_time.
+        Each status is written to DB"""
         answer_time = answer_time
         update_rec_at = models.GlobalResults.objects.filter(call_no=call.name, run=self.simulation_number+1).last()
         update_rec_at.answer_time = answer_time
@@ -279,63 +358,18 @@ class CallCenter:
         update_rec_at.save()
 
     def run(self):
+        """The method is responsible for creating a simpy process and running the simulation environment"""
         self.env.process(self.dial())
         self.env.run()
 
 
 class IncomingCall:
+    """This class creates an instance of a customer, which is passed to the CallCenter class to simulate the call
+    center business process. It should be noted that the IncomingCall class is a part of the simulation and not an
+    actual representation of a customer in a call center. The name attribute represents the phone number of the
+    customer, and the patience attribute represents the amount of time the customer is willing to wait on hold before
+     hanging up"""
     def __init__(self, name):
         self.name = name
         self.patience = random.uniform(models.CreateInput.objects.values('p_h').last()['p_h'],
                                        models.CreateInput.objects.values('p_l').last()['p_l'])
-
-
-def launch(request):
-
-    start = datetime.now()
-    for simulation_number in range(models.CreateInput.objects.values('number_of_simulations').last()['number_of_simulations']):
-        print(f'run {simulation_number}')
-        CallCenter.env = simpy.Environment()
-        call_center = CallCenter(simulation_number, num.id)
-        call_center.run()
-    finish = datetime.now()
-    print(f' simulation took {finish - start}')
-    return redirect('simulator:export')
-
-
-
-
-def display_results(request):
-    template_name = 'simulator/export.html'
-    myvars = models.CreateInput.objects.filter(uuid=num.id).values().last()
-    fields = InputForm()
-    return render(request,  template_name=template_name, context={'myvars': myvars, 'form': fields})
-
-
-def export_csv(request):
-
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="simulation_log.csv"'
-
-    writer = csv.writer(response)
-    writer.writerow(['shift_started', 'shift_finished','run', 'attempt_no', 'call_no', 'batch', 'queue', 'spin', 'capacity', 'attempt_started', 'if_unreachable',
-                     'if_not_answering', 'answer_time', 'amd_time', 'if_dropped', 'wait_before_drop', 'wait_time',
-                     'talk_time', 'clerical_time'])
-
-    results = models.GlobalResults.objects.filter(uuid=num.id).values_list('shift_started', 'shift_finished', 'run', 'attempt_no', 'call_no', 'batch', 'queue', 'spin', 'capacity',
-                                                           'attempt_started', 'if_unreachable',
-                                                           'if_not_answering', 'answer_time', 'amd_time', 'if_dropped',
-                                                           'wait_before_drop', 'wait_time', 'talk_time',
-                                                           'clerical_time')
-
-    for result in results:
-        writer.writerow(result)
-
-    models.GlobalResults.objects.filter(uuid=num.id).delete()
-    models.CreateInput.objects.filter(uuid=num.id).delete()
-
-    return response
-
-
-
-
