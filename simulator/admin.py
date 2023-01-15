b32b5de6efc1400bc12b1a53c0c1af33446b5ea3
ygrachov <iurii.grachov@gmail.com>
2023-01-15 09:01:21 +0100

    superuser & google analytics

--- simulator/admin.py
+++ simulator/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
+from .import models
 
-# Register your models here.
+class CreateInputAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.CreateInput._meta.fields if field.name != "id"]
+
+class GlobalResultsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.GlobalResults._meta.fields if field.name != "id"]
+
+admin.site.register(models.CreateInput, CreateInputAdmin)
+admin.site.register(models.GlobalResults, GlobalResultsAdmin)
